98a003cceb34907ae6b1cca154ff5461fec81656
JunaidHafeez21 <160257992+JunaidHafeez21@users.noreply.github.com>
2024-02-17 19:14:50 +0500

    Add files via upload

---
+++ Qno1.js
@@ -0,0 +1,5 @@
+"use strict";
+//  Q1: Install Node.js, TypeScript and VS Code on your computer.
+// 1. [Install nodejs] (https://nodejs.org/en/download)
+// 2. [Install typescript] (npm install -g typescript)
+// 3. [Install VS Code] (https://code.visualstudio.com/)
---
+++ Qno1.ts
@@ -0,0 +1,6 @@
+//  Q1: Install Node.js, TypeScript and VS Code on your computer.
+
+// 1. [Install nodejs] (https://nodejs.org/en/download)
+// 2. [Install typescript] (npm install -g typescript)
+// 3. [Install VS Code] (https://code.visualstudio.com/)
+
